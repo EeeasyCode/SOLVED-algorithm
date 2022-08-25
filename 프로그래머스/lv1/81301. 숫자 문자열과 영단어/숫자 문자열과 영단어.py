73ecc42675050416e651612982d176336c7871a6
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-08-26 00:58:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.28 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,11 @@
+import re
+
+def solution(s):
+    answer = 0
+    num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i in range(10):
+        s = re.sub(num[i], str(i), s)
+    answer = int(s)
+    
+    
+    return answer
