9be5c18b60f264d9d16fbdc43764ac3b8af2dd46
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-08-26 01:04:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,10 +1,8 @@
-import re
-
 def solution(s):
     answer = 0
-    num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(10):
-        s = re.sub(num[i], str(i), s)
+    num_dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for key, value in num_dic.items():
+        s = s.replace(key, value)
     answer = int(s)
     
     
