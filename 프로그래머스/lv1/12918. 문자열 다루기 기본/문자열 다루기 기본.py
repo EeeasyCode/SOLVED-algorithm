86762c9f3ba0c22d05e35fbb2bec36042482a17c
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-09-01 20:17:28 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    
+    if (len(s)== 4 or len(s) == 6):
+        if s.isdigit():
+            return True
+        else:
+            return False
+    
+    else:
+        answer = False
+        
+    return answer
