51d2a55dc9bb9d7bf08c95f14aa31b734030631c
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2023-01-18 17:54:46 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = ''
+    
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    
+    answer = str(int(''.join(numbers)))
+        
+        
+    return answer
