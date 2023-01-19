392c74d1b10201e5fb85c2f164c6eae06c7bb1f2
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2023-01-19 19:32:27 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1200 ms, Memory: 40964 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -0,0 +1,24 @@
+import sys
+
+prime_num = []
+check = [0] * 1000001
+check[0] = 1
+check[1] = 1
+
+for i in range(2, 1000001):
+    if check[i] == 0:
+        prime_num.append(i)
+        for j in range(2*i, 1000001, i):
+            check[j] = 1
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    count = 0
+    N = int(sys.stdin.readline())
+    for i in prime_num:
+        if i >= N:
+            break
+        if not check[N - i] and i <= N-i:  
+            count += 1
+    print(count)
