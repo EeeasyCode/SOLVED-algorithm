2818ffaac9fec2ef7bed37fe5602fdb7530d32fd
changmin Lee / 이창민 <EeeasyCode@gmail.com>
2024-01-08 15:43:45 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+arr = [0] * n
+for i in range(n):
+  arr[i] = i+1
+
+for _ in range(m):
+  a, b = map(int, input().split())
+  arr[a-1], arr[b-1] = arr[b-1], arr[a-1]
+
+for i in arr:
+  print(i, end=' ')
