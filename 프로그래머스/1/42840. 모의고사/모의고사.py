f7ed5f78a76ba64ebfcb6be385f3abf53212b25a
changmin Lee / 이창민 <EeeasyCode@gmail.com>
2024-01-09 15:21:37 +0900

    [level 1] Title: 모의고사, Time: 3.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,21 @@
 def solution(answers):
-    answer = [0 for i in range(3)]
-
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
+    answer = [0] * 3
+    one_arr = [1,2,3,4,5]
+    two_arr = [2,1,2,3,2,4,2,5]
+    three_arr = [3,3,1,1,2,2,4,4,5,5]
     
     for i in range(len(answers)):
-        ans = answers[i]
-        if(man1[i%len(man1)] == ans):
+        if (answers[i] == one_arr[i%len(one_arr)]):
             answer[0] += 1
-        if(man2[i%len(man2)] == ans):
+        if (answers[i] == two_arr[i%len(two_arr)]):
             answer[1] += 1
-        if(man3[i%len(man3)] == ans):
-            answer[2] += 1     
+        if (answers[i] == three_arr[i%len(three_arr)]):
+            answer[2] += 1
     
-    result = []
+    max_val = max(answer)
+    high_score = []
     for i in range(len(answer)):
-        if(answer[i] == max(answer)):
-            result.append(i+1)
+        if max_val == answer[i]:
+            high_score.append(i+1)
     
-    return sorted(result)
+    return sorted(high_score)
