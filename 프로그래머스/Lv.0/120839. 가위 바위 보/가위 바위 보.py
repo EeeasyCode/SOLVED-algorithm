8e209f321d7765242bc985b455f8b3d291fd95da
changmin Lee / 이창민 <EeeasyCode@gmail.com>
2024-01-15 15:34:13 +0900

    [level Lv.0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,11 @@
+def solution(rsp):
+    answer = ''
+    for r in rsp:
+        if r == "2":
+            answer += "0"
+        elif r == "0":
+            answer += "5"
+        else:
+            answer += "2"
+            
+    return answer
