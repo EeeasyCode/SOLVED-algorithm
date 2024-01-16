13ec0348493cda464f6ec909f1f1799944273921
changmin Lee / 이창민 <EeeasyCode@gmail.com>
2024-01-16 15:50:38 +0900

    [level Lv.1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == [1,2,3,1]:
+            answer += 1
+            for _ in range(4):
+                stack.pop()
+    return answer
