015e1836e5eeb9fd230067e7fef6428b702aad50
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-07 18:31:18 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
 
 for _ in range(t):
-  n = int(input())
-  dp = [0] * (n+1)
-  
-  for i in range(0, n):
-    if i==0:
-      dp[i]=1
-    elif i==1:
-      dp[i]=2
-    elif i==2:
-      dp[i]=4
-    else:
-      dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
-  print(dp[n-1])
+
+	n = int(input())
+	dp = [0, 1, 2, 4]
+
+	for i in range(4, n+1):
+		dp.append(dp[i-1] + dp[i-2] + dp[i-3])
+	print(dp[n])
