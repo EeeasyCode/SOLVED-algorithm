0488b16bec38357c420d15a89ac063671cc80172
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-12 02:52:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,18 +1,9 @@
 def solution(nums):
-    answer = {}
-    for num in nums:
-        if num in answer:
-            answer[num] += 1
-        else:
-            answer[num] = 1
-    cnt = 0
-    a = []
-    for k, v in answer.items():
-        if cnt == len(nums)//2:
-            break
-        if v != 0:
-            a.append(k)
-            answer[k] -= 1
-            cnt += 1
-    print(a)
-    return len(list(set(a)))
+    answer = len(set(nums)) 
+    n = len(nums) // 2
+    if answer < n:
+        return answer
+    else:
+        return n
+    
+    
