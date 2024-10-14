e6b0810130fabda17a2733de58b5408425b4a80d
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-14 12:20:41 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 18:15:6
+2024년 10월 14일 12:20:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,18 @@
+from functools import reduce
+import operator
+
 def solution(clothes):
     answer = 1
-    dict = {}
-    for clothe in clothes:
-        c_name, c_type = clothe
-        if c_type in dict:
-            dict[c_type] += 1
-        else:
-            dict[c_type] = 1
+    clothe_dict = {}
     
-    for v in dict.values():
-        answer = answer * (v+1)
+    for clothe in clothes:
+        if clothe[1] in clothe_dict.keys():
+            clothe_dict[clothe[1]] += 1
+        else:
+            clothe_dict[clothe[1]] = 1
+
+    
+    for _, v in clothe_dict.items():
+        answer *= (v+1)
         
     return answer - 1
