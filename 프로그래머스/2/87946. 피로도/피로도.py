6169f9e96e3ac343f2cbe6896294c7ad4f1a5ba6
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-18 17:39:26 +0900

    [level 2] Title: 피로도, Time: 60.43 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,44 @@
 answer = 0
-
-def recur(k, dungeons, count, visit):
+def dfs(k, visit, count, dungeons):
     global answer
     
     if answer < count:
         answer = count
         
     for i, dungeon in enumerate(dungeons):
-        if not visit[i] and k >= dungeon[0]:
-            visit[i] = True
-            recur(k-dungeon[1], dungeons, count+1, visit)
-            visit[i] = False
-
+        if visit[i] != 1 and k >= dungeon[0]:
+            visit[i] = 1
+            dfs(k-dungeon[1], visit, count+1, dungeons)
+            visit[i] = 0
+    
+    
 def solution(k, dungeons):
     global answer
+    visit = [0] * len(dungeons)
     count = 0
-    visit = [False] * len(dungeons)
-    recur(k, dungeons, count, visit)
+    
+    dfs(k, visit, count, dungeons)
     
     return answer
+
+# answer = 0
+
+# def recur(k, dungeons, count, visit):
+#     global answer
+    
+#     if answer < count:
+#         answer = count
+        
+#     for i, dungeon in enumerate(dungeons):
+#         if not visit[i] and k >= dungeon[0]:
+#             visit[i] = True
+#             recur(k-dungeon[1], dungeons, count+1, visit)
+#             visit[i] = False
+
+# def solution(k, dungeons):
+#     global answer
+#     count = 0
+#     visit = [False] * len(dungeons)
+#     recur(k, dungeons, count, visit)
+    
+#     return answer
