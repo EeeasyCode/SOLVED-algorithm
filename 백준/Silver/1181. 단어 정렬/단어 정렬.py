92d39d0bbaf8944bdb6f90e1a77b616f2b268e60
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-12 15:33:52 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,8 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 str_list = []
 
 for _ in range(N):
-    str_list.append(input())
+    str_list.append(input().strip())
 
 str_list = list(set(str_list))
 answer = sorted(str_list, key = lambda x : (len(x), x))
