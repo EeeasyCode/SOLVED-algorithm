fda53348fdef0f6b1e1ec361da55df722edbbfd5
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-18 00:35:21 +0900

    [Silver V] Title: 방 번호, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,13 +1,17 @@
-word = input()
-ans = [0] * 10
-for i in range(len(word)):
-    num = int(word[i])
-    if num == 6 or num == 9:
-        if ans[6] <= ans[9]:
-            ans[6] += 1
+import sys
+input = sys.stdin.readline
+
+num = input().rstrip()
+
+set_dict = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 }
+
+for i in num:
+    if i == '6' or i == '9':
+        if set_dict[6] >= set_dict[9]:
+            set_dict[9] += 1
         else:
-            ans[9] += 1
+            set_dict[6] += 1
     else:
-        ans[num] += 1
- 
-print(max(ans))
+        set_dict[int(i)] += 1
+
+print(max(set_dict.values()))
