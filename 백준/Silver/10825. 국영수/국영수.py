be4f0828591a898be0e8ab0c042285c87f847fad
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-30 15:42:00 +0900

    [Silver IV] Title: 국영수, Time: 396 ms, Memory: 69980 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,17 +1,18 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 
 n = int(input())
 
 # 이름 국어 영어 수학
-
-score_list = []
+score_dict = {}
 
 for _ in range(n):
-    score_list.append(input().split())
+    name, korean, english, math = input().split()
+    score_dict[name] = (int(korean), int(english), int(math))
 
-score_list.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+# 딕셔너리의 items를 정렬
+sorted_scores = sorted(score_dict.items(), key=lambda x: (-x[1][0], x[1][1], -x[1][2], x[0]))
 
-for name in score_list:
-    print(name[0])
+# 결과 출력
+for name, _ in sorted_scores:
+    print(name)
