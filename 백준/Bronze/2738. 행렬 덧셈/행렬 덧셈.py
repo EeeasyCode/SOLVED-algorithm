bbc7359bba33981f2785f7625eb5403edf9f1fd1
Khan / 이창민 <EeeasyCode@gmail.com>
2025-04-16 22:36:01 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,13 +1,15 @@
-a_arr, b_arr = [], []
-n, m = map(int, input().split())
+# N*M 크기의 행렬 A, B -> 더한 행렬
+# 입력 -> N, M (행렬의 크기)
+# 입력 -> 행렬 A -> 행렬 B
+N, M = map(int, input().split())
+answer_arr = [[0 for _ in range(M)] for _ in range(N)]
 
-for row in range(n):
-  a_arr.append(list(map(int, input().split())))
+a_arr = [ list(map(int, input().split())) for _ in range(N) ]
+b_arr = [ list(map(int, input().split())) for _ in range(N) ]
 
-for row in range(n):
-  b_arr.append(list(map(int, input().split())))
+for row in range(N):
+  for col in range(M):
+    answer_arr[row][col] = a_arr[row][col] + b_arr[row][col]
 
-for row in range(n):
-  for col in range(m):
-    print(a_arr[row][col]+b_arr[row][col], end=' ')
-  print()
+for data in answer_arr:
+  print(*data)
