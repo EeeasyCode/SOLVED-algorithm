a4e3e7afb41c7ed6f1a1a589d3a5b90cfaa3b4b2
Khan / 이창민 <EeeasyCode@gmail.com>
2025-04-18 00:45:04 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,17 +1,23 @@
 N = int(input())
 answer = 0
-
-def check_group_word(str_arr):
-  char_list = {}
-  for idx, c in enumerate(str_arr):
-    if c in char_list:
-      if char_list[c] != idx - 1:
-        return 0
-    char_list[c] = idx
-  return 1
-
 for _ in range(N):
-  str_arr = input()
-  answer += check_group_word(str_arr)
+  word = input()
+  prev = word[0]
+  isNotGroupWord = False
+
+  for i in range(1, len(word)):
+    if word[i] == prev:
+      continue
+    
+    if prev in word[i:]:
+      isNotGroupWord = True
+      break
+
+    else:
+      prev = word[i]
+  
+  if not isNotGroupWord:
+    answer += 1
 
 print(answer)
+ 
